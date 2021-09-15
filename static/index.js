2d4d1e2646e2f697e266076f75214be876cde2db
Leone Oliveira <falecom@leone.tec.br>
2021-09-15 19:03:48 -0300

    Adicionada validação via JavaScript dos formulários

--- static/index.js
+++ static/index.js
@@ -1,3 +1,20 @@
 function redirect(url) {
   window.location.href = url
 }
+
+(function () {
+  'use strict'
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false)
+    });
+})()
